8b8a28c9ab7721d7b59d88a0f95cf2b89cdc5c9d
Shubham Rai <shubhamrai01683@gmail.com>
2025-02-28 13:03:16 +0530

    Challenge 01

---
+++ src/Challenges/ArraySum.java
@@ -0,0 +1,16 @@
+package Challenges;
+
+public class ArraySum {
+    public static void main(String[] args) {
+        int[] a= {5,7,6,9,8,1,-15,2,6};
+
+        int sum=0;
+        System.out.print("Array elements: ");
+        for (int num : a){
+            System.out.println(num+ " ");
+            sum += num;
+        }
+
+        System.out.println("\nSum of Array Elements: "+sum);
+    }
+}
