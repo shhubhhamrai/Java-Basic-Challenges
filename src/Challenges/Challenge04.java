e5081b810edf044e9c7c3bb94d952b185a553ddb
Shubham Rai <shubhamrai01683@gmail.com>
2025-02-28 15:19:38 +0530

    Initial Changes

---
+++ src/Challenges/Challenge04.java
@@ -0,0 +1,17 @@
+package Challenges;
+
+public class Challenge04 {
+    public static void main(String[] args) {
+        // Define the string
+        String str = "Welcome to Clarivate!";
+
+        // Calculate and print length of string
+        int length = calculateStringLength(str);
+        System.out.println("Length of the string: " + length);
+    }
+
+    // Method to calculate length of a string
+    public static int calculateStringLength(String str) {
+        return str.length();
+    }
+}
